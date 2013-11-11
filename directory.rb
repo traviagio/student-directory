c3576034d07dd3f598d02101e8fc7e40aff674c8
Georgi Georgiev <georgiev.georgio@gmail.com>
2013-11-11 17:08:57 +0000

    improving the verson so we can get the imput from users

--- directory.rb
+++ directory.rb
@@ -1,43 +1,38 @@
-# let's put all students into an array
-students = [
-{:name => "Georgi Georgiev", :cohort => :november},
-{:name => "JB", :cohort => :november},
-{:name => "Ken", :cohort => :november},
-{:name => "Aneth", :cohort => :november},
-{:name => "Erica", :cohort => :november},
-{:name => "Chris", :cohort => :november},
-{:name => "Kumy", :cohort => :november},
-{:name => "Lara", :cohort => :november},
-{:name => "James", :cohort => :november},
-{:name => "NightPain", :cohort => :november},
-{:name => "James", :cohort => :november},
-{:name => "NightPain", :cohort => :november},
-{:name => "James", :cohort => :november},
-{:name => "Jeremy", :cohort => :november},
-{:name => "Giacomo", :cohort => :november},
-{:name => "Nisar", :cohort => :november},
-{:name => "Michael", :cohort => :november},
-{:name => "Peter", :cohort => :november},
-]
-
 def print_header
-puts "The students of my cohort at Makers Academy"
-puts "_________________"
+	puts "The students of my cohort at Makers Academy"
+	puts "________________"
 end
-
 
 def print(students)
 	students.each do |student|
-	puts "#{student[:name]} (#{student[:cohort]} cohort)"
- end
+		puts "#{student[:name]} (#{student[:cohort]} cohort)"
+	end
 end
 
 def print_footer(names)
 puts "Overall, we have #{names.length} great students"
-end
+end		
 
-#nothing happens until we call methods
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, just hit the return twice"
+	#create an empty array
+	students = []
+    # get the first name
+    name = gets.chomp
+    #while the name is not empty, repeat this code
+    while !name.empty? do
+    	#add the student hash to the array
+    	students << {:name => name, :cohort => :novermber}
+    	puts "Now we have #{students.length} students"
+    	# get another name from the user
+    	name = gets.chomp
+    end
+    #return the array of students
+    students
+ end
 
-print_header
-print(students)
-print_footer(students)
+ students = input_students
+ print_header
+ print(students)
+ print_footer(students)
