e3ebe8c60290ac1479e29adb8dca937c81e9dba8
Georgi Georgiev <georgiev.georgio@gmail.com>
2013-11-11 16:08:13 +0000

    added new comments to the file

--- directory.rb
+++ directory.rb
@@ -1,13 +1,24 @@
+# first, we print the list of stuents
 puts "The students of my cohort at Makers Academy"
+puts "_________________"
 puts "Georgi Georgiev"
-puts 
-puts
-puts
-puts
-puts
-puts
-puts
-puts
-puts
-puts
-puts
+puts "JB"
+puts "Ken"
+puts "Aneth"
+puts "Erica"
+puts "Chris"
+puts "Kumy"
+puts "Lara"
+puts "James"
+puts "NightPain"
+puts "James"
+puts "Jeremy"
+puts "Giacomo"
+puts "Nisar"
+puts "Michael"
+puts "Peter"
+#finally, we print the total
+print "Overall, we have "
+#it's important that print() doesn't add new line characters
+print 9
+puts " great students"
