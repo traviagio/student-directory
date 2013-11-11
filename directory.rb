a0513d848e0f3a849371813cf99843bc09646cda
Georgi Georgiev <georgiev.georgio@gmail.com>
2013-11-11 16:38:28 +0000

    extracted code into three methods fixed error

--- directory.rb
+++ directory.rb
@@ -33,7 +33,7 @@ def print(names)
 end
 
 def print_footer(names)
-puts "Overall, we have #{students.length} great students"
+puts "Overall, we have #{names.length} great students"
 end
 
 #nothing happens until we call methods
