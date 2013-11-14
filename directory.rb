ade0a8bc2d9e8405970d08f74c9d52ee8c2388d6
Georgi Georgiev <georgiev.georgio@gmail.com>
2013-11-14 12:26:14 +0000

    Showing country

--- directory.rb
+++ directory.rb
@@ -1,39 +1,38 @@
 def print_header
-	puts "The students of my cohort at Makers Academy"
-	puts "________________"
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
 end
 
 def print(students)
-    # puts students.inspect
-    students.select{ |student| student[:name].length < 11 }.each do |student|
-        puts student[:name]
-    end
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort) from (#{student[:country]})"
+  end
 end
 
-def print_footer(names)
-puts "Overall, we have #{names.length} great students"
-end		
+def print_footer(names)    
+  puts "Overall, we have #{names.length} great students"
+end
 
 def input_students
-	puts "Please enter the names of the students"
-	puts "To finish, just hit the return twice"
-	#create an empty array
-	students = []
-    # get the first name
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do    
+    # add the student hash to the array
+    students << {:name => name, :cohort => :november, :country => :UK}    
+    puts "Now we have #{students.length} students"
+    # get another name from the user
     name = gets.chomp
-    #while the name is not empty, repeat this code
-    while !name.empty? do
-    	#add the student hash to the array
-    	students << {:name => name, :cohort => :novermber}
-    	puts "Now we have #{students.length} students"
-    	# get another name from the user
-    	name = gets.chomp
-    end
-    #return the array of students
-    students
- end
+  end
+  # return the array of students
+  students
+end
 
- students = input_students
- print_header
- print(students)
- print_footer(students)
+students = input_students
+print_header
+print(students)
+print_footer(students)
