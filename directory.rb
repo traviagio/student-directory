88cc7838faafdce76938aec04a74d95e1304e339
Georgi Georgiev <georgiev.georgio@gmail.com>
2013-11-14 14:14:23 +0000

    Added a center() which places puts in the middle

--- directory.rb
+++ directory.rb
@@ -1,21 +1,25 @@
 def print_header
-  puts "The students of my cohort at Makers Academy"
-  puts "-------------"
+  puts_center "The students of my cohort at Makers Academy" 
+  puts_center "-------------"
 end
 
 def print(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort) from (#{student[:country]})"
+    puts_center "#{student[:name]} (#{student[:cohort]} cohort) from (#{student[:country]})"  
   end
 end
 
 def print_footer(names)    
-  puts "Overall, we have #{names.length} great students"
+  puts_center "Overall, we have #{names.length} great students" 
+end
+
+def puts_center(string)
+    puts string.center(140)
 end
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts_center "Please enter the names of the students"
+  puts_center "To finish, just hit return twice" 
   # create an empty array
   students = []
   # get the first name
@@ -23,8 +27,8 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do    
     # add the student hash to the array
-    students << {:name => name, :cohort => :november, :country => :UK}    
-    puts "Now we have #{students.length} students"
+    students << {:name => name, :cohort => :november, :country => :UK}  
+    puts_center "Now we have #{students.length} students"
     # get another name from the user
     name = gets.chomp
   end
