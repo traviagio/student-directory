dcb0e7483b49deb932b21e2984b9b44d490cb4c6
Georgi Georgiev <georgiev.georgio@gmail.com>
2013-11-15 19:00:53 +0000

    Added separate methods fro getting name, cohort and country

--- directory.rb
+++ directory.rb
@@ -9,6 +9,13 @@ def print(students)
   end
 end
 
+#def print_grouped_students(students)
+ #   puts_center cohort  
+  #students.map { |student| student[:cohort] }
+   # puts_center "#{student[:name]} from (#{student[:country]})"  
+#end
+
+
 def print_footer(names)    
   puts_center "Overall, we have #{names.length} great students" 
 end
@@ -16,6 +23,29 @@ end
 def puts_center(string)
     puts string.center(140)
 end
+
+def print_country(students,country)
+  if students.length == 1
+    puts_center "Now we have #{students.length} student from " + country.to_s
+  else
+    puts_center "Now we have #{students.length} students from " + country.to_s
+  end
+end
+
+def get_name
+  puts "Please enter your name:"
+  gets.chomp
+end
+
+def get_cohort
+  puts "Please enter your cohort:"
+  gets.chomp
+end  
+
+def get_country
+  puts "Please enter your country:"
+  gets.chomp
+end  
 
 def input_students
   puts_center "Please enter the names of the students"
@@ -26,31 +56,31 @@ def input_students
   # while the name is not empty, repeat this code
   
   loop do 
-    puts "Please enter your name:"
-       name = gets.chomp
-       break if name.empty?
-     puts "Please enter your cohort:"
-       cohort = gets.chomp 
-       break if cohort.empty?  
-     puts "Please enter your country:"
-       country = gets.chomp
-       break if country.empty?
-       student = {:name => name, :cohort => cohort, :country => country}
-       students << student
-       if students.length == 1
-       puts_center "Now we have #{students.length} student from " + country.to_s
-       else
-       puts_center "Now we have #{students.length} students from " + country.to_s
-       end
-       puts_center "Please continue if you wish to add more students." 
-       puts_center "To exit, please hit return twice!" 
-  end  
-  
+      name = get_name
+      break if name.empty?
+      
+      cohort = get_cohort 
+      break if cohort.empty?
 
+      
+      country = get_country
+      break if country.empty?
+
+      student = {:name => name, :cohort => cohort, :country => country}
+      students << student
+      print_country(students,country)
+      puts_center "Please continue if you wish to add more students." 
+      puts_center "To exit, please hit return twice!" 
+  end  
+ 
   students
 end
 
+
+
+
 students = input_students
 print_header
-print(students)
+#print(students)
+#print_grouped_students(students)
 print_footer(students)
